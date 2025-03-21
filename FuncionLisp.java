463c2f46cbe912b1d2788df87bf6158cacc07759
Martinho777 <martinguille17@gmail.com>
2025-03-20 18:42:03 -0600

    Update FuncionLisp.java
    comentarios descriptivos

--- FuncionLisp.java
+++ FuncionLisp.java
@@ -1,16 +1,21 @@
+/**
+ * Clase FuncionLisp.
+ * Representa una función definida por el usuario en Lisp.
+ * Almacena el nombre, la lista de parámetros y el cuerpo de la función.
+ */
 import java.util.List;
 
 public class FuncionLisp {
     private String nombre;
     private List<String> parametros;
     private Object cuerpo;
-
+//constructor de funcion
     public FuncionLisp(String nombre, List<String> parametros, Object cuerpo) {
         this.nombre = nombre;
         this.parametros = parametros;
         this.cuerpo = cuerpo;
     }
-
+//sets y gets
     public String getNombre() { return nombre;}
 
     public List<String> getParametros() {return parametros;}
